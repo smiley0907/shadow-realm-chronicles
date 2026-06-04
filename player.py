cd2aefad4c64993ae668b08a878a9b4bdc52a033
PurnachandraRao <pcr.bobbepalli@gmail.com>
2026-06-04 06:35:41 +0530

    Create player.py

---
+++ player.py
@@ -0,0 +1,74 @@
+from direct.task import Task
+
+
+class Player:
+
+    def __init__(self, base):
+
+        self.base = base
+
+        self.player = None
+
+        self.speed = 0.5
+
+    def load_player(self):
+
+        self.player = self.base.loader.loadModel(
+            "models/panda"
+        )
+
+        self.player.reparentTo(
+            self.base.render
+        )
+
+        self.player.setScale(0.5)
+
+        self.player.setPos(
+            0,
+            0,
+            0
+        )
+
+        self.register_keys()
+
+    def register_keys(self):
+
+        self.base.accept(
+            "arrow_up",
+            self.move_forward
+        )
+
+        self.base.accept(
+            "arrow_down",
+            self.move_backward
+        )
+
+        self.base.accept(
+            "arrow_left",
+            self.move_left
+        )
+
+        self.base.accept(
+            "arrow_right",
+            self.move_right
+        )
+
+    def move_forward(self):
+        self.player.setY(
+            self.player.getY() + self.speed
+        )
+
+    def move_backward(self):
+        self.player.setY(
+            self.player.getY() - self.speed
+        )
+
+    def move_left(self):
+        self.player.setX(
+            self.player.getX() - self.speed
+        )
+
+    def move_right(self):
+        self.player.setX(
+            self.player.getX() + self.speed
+        )
